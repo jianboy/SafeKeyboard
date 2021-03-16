46e1b45555d73823c6f737b2374f0fbf110f71fe
liuyuqi-dellpc <liuyuqi.gov@msn.cn>
2021-03-16 18:51:58 +0800

    v1

--- app/src/main/java/me/yoqi/android/safekeyboard/MainActivity.java
+++ app/src/main/java/me/yoqi/android/safekeyboard/MainActivity.java
@@ -1,14 +1,29 @@
 package me.yoqi.android.safekeyboard;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import me.yoqi.android.safekeyboard.keyboard.KeyBoardDialogUtils;
 
 public class MainActivity extends AppCompatActivity {
+    private KeyBoardDialogUtils keyBoardDialogUtils;
+    private EditText et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et = (EditText) findViewById(R.id.et);
+        keyBoardDialogUtils = new KeyBoardDialogUtils(this);
+        et.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                keyBoardDialogUtils.show(et);
+            }
+        });
     }
 }
